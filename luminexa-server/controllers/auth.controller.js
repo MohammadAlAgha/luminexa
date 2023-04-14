87cec67e20e9dc41671fec6529b3e5051bd3768b
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-14 22:45:43 +0300

    Connected the server to the data base

---
+++ luminexa-server/controllers/auth.controller.js
@@ -0,0 +1,39 @@
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { email, password, userType, userName } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser)
+    return res.status(409).json({ message: "Email already exists" });
+
+  const user = new User();
+  user.email = email;
+  user.password = password;
+  user.userName = userName;
+
+  if (userType) user.userType = userType;
+  await user.save();
+  const { password: hashedPassword, ...newUser } = user.toJSON();
+  res.status(201).json(newUser);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+
+  const isMatched = user.matchPassword(password);
+  if (!isMatched)
+    return res.status(404).json({ message: "Invalid Credentials" });
+
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.SECRET_KEY
+  );
+
+  res.json({ token });
+};
