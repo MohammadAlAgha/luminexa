b12c7a228f79011b5457695abb1a6f573a75da02
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-14 22:50:12 +0300

    Fully Created the login & register APIs

--- luminexa-server/index.js
+++ luminexa-server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+app.use(express.json());
+
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
 
 app.listen(process.env.PORT, (error) => {
   if (error) console.error(error);
   console.log(`Server Running on Port ${process.env.PORT}`);
+  require("./configs/db.configs");
 });
