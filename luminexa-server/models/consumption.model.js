339f642871eda5e4a21d2c93dcf9a7cea9a5e3ef
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-14 17:56:02 +0300

    Added the consumption history schema

---
+++ luminexa-server/models/consumption.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const consumptionSchema = new mongoose.Schema({
+  time: {
+    type: Date,
+    required: true,
+  },
+  intensity: {
+    type: Number,
+    required: true,
+  },
+  currentConsumption: {
+    type: Number,
+    required: true,
+  },
+});
