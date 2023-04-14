a240083af87b85917315de1c1dc4ecdef7564e6c
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-14 21:50:03 +0300

    Embedded the led,mode and schedule models in the system model

--- luminexa-server/models/system.model.js
+++ luminexa-server/models/system.model.js
@@ -5,6 +5,19 @@ const systemSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  leds: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Led",
+    required: true,
+  },
+  mode: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Mode",
+  },
+  schedule: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Schedule",
+  },
 });
 
 const System = mongoose.model("System", systemSchema);
