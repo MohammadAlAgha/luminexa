e17b3917aa648a86a284cb5facda0b4bacc4f759
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-15 21:45:38 +0300

    Fixed the get leds API

--- luminexa-server/controllers/leds.controller.js
+++ luminexa-server/controllers/leds.controller.js
@@ -2,7 +2,7 @@ const Led = require("../models/led.model");
 const System = require("../models/system.model");
 
 exports.getLeds = async (req, res) => {
-  const { systemId } = res.body;
+  const { systemId } = req.body;
   const system = await System.findById(systemId);
 
   res.json(system.leds);
