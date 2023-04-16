bc9af9b0248839607af12ab2189017899a98da94
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-16 14:05:06 +0300

    Embedded the schedule schema correctly in the system schema

--- luminexa-server/models/system.model.js
+++ luminexa-server/models/system.model.js
@@ -1,7 +1,39 @@
 const mongoose = require("mongoose");
 const ledsSchema = require("../models/led.model");
 const modesSchema = require("../models/mode.model");
-const schedulesSchema = require("../models/schedule.model");
+
+const scheduleSchema = new mongoose.Schema({
+  scheduleTitle: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: Date,
+    required: true,
+  },
+  repeat: {
+    type: [
+      {
+        type: String,
+        enum: [
+          "monday",
+          "tuesday",
+          "wednesday",
+          "thursday",
+          "friday",
+          "saturday",
+          "sunday",
+        ],
+      },
+    ],
+    default: [],
+  },
+  scheduleStatus: {
+    type: String,
+    enum: ["on", "off"],
+    default: "on",
+  },
+});
 
 const systemSchema = new mongoose.Schema({
   systemName: {
@@ -15,7 +47,7 @@ const systemSchema = new mongoose.Schema({
   },
   leds: [ledsSchema],
   modes: [modesSchema],
-  schedules: [schedulesSchema],
+  schedules: [scheduleSchema],
   users: [
     {
       type: mongoose.Schema.Types.ObjectId,
