92e585ffe1ac849c9d6e2280b2ec528e8a2a9a24
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-19 22:42:03 +0300

    Added regex for the register API

--- luminexa-server/controllers/auth.controller.js
+++ luminexa-server/controllers/auth.controller.js
@@ -5,26 +5,43 @@ const bcrypt = require("bcrypt");
 exports.register = async (req, res) => {
   const { email, password, userName } = req.body;
 
+  // Validating the email using regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+
+  //Validating the password using regex
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  if (!passwordRegex.test(password)) {
+    return res.status(400).json({
+      message:
+        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number",
+    });
+  }
+
   const existingUser = await User.findOne({ email });
 
-  if (existingUser)
+  if (existingUser) {
     return res.status(409).json({ message: "Email already exists" });
+  } //checking of the email address exists
 
   const saltRounds = 10;
   const salt = await bcrypt.genSalt(saltRounds);
-  const hashed = await bcrypt.hash(password, salt);
+  const hashed = await bcrypt.hash(password, salt); //Hashing the password before saving
 
   const user = new User();
   user.email = email;
   user.password = hashed;
   user.userName = userName;
+  //creating the new user
 
   await user.save();
 
   const token = jwt.sign(
     { id: user._id, email: user.email },
     process.env.SECRET_KEY
-  );
+  ); //generating a token
 
   const { password: hashedPassword, ...newUser } = user.toJSON();
 
@@ -33,18 +50,21 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
 
-  if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+  const user = await User.findOne({ email }); //finding user by email
+
+  if (!user) {
+    return res.status(404).json({ message: "Invalid Credentials" });
+  } //checking if the user exists after checking searching for his email
 
   const isMatched = await bcrypt.compare(password, user.password);
   if (!isMatched)
-    return res.status(404).json({ message: "Invalid Credentials" });
+    return res.status(404).json({ message: "Invalid Credentials" }); //comparing the entered password with the registered hashed password peviously entered by the user
 
   const token = jwt.sign(
     { id: user._id, email: user.email },
     process.env.SECRET_KEY
-  );
+  ); //generating a token
 
   res.json({ token });
 };
