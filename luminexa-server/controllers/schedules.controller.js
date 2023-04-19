96eba9d8e7e28b57d6689ec95cd13d0b14319e21
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-20 02:06:12 +0300

    Edited all the schedule APIs & updated the last manual when needed

--- luminexa-server/controllers/schedules.controller.js
+++ luminexa-server/controllers/schedules.controller.js
@@ -3,20 +3,25 @@ const System = require("../models/system.model");
 exports.addSchedule = async (req, res) => {
   const { systemId, scheduleTitle, time, repeat } = req.body;
 
-  const schedule = {
-    scheduleTitle,
-    time,
-    repeat,
-  };
-  //creating the new schedule form
-
   const system = await System.findById(systemId); //getting the system by ID
 
   if (!system) {
     return res.status(404).json({ message: "System not found" });
   } //checking if system exists
 
+  const leds = system.leds; //getting the cuurent leds status
+
+  const schedule = {
+    scheduleTitle,
+    time,
+    repeat,
+    leds,
+  };
+  //creating the new schedule form with the current led status
+
   system.schedules.push(schedule); //adding the new schedule to the system
+
+  system.leds = system.lastManual; //seting back the leds as the last manual (before setting the schedule)
 
   await system.save();
 
@@ -50,6 +55,10 @@ exports.toggleSchedule = async (req, res) => {
 
   schedule.scheduleStatus = schedule.scheduleStatus == "on" ? "off" : "on"; //toggling the schedule from on to off or from off to on
 
+  if (schedule.scheduleStatus == "off") {
+    system.leds = system.lastManual;
+  } //Set the modes back to the old status after the schedule is off
+
   await system.save();
 
   res.json(system);
@@ -72,10 +81,15 @@ exports.updateSchedule = async (req, res) => {
     return res.status(404).json({ message: "Schedule not found" });
   } //checking if schedule exists
 
+  const leds = system.leds; //getting the cuurent leds status
+
   schedule.scheduleTitle = scheduleTitle;
   schedule.time = time;
   schedule.repeat = repeat;
+  schedule.leds = leds;
   //updating the schedule with the new values
+
+  system.leds = system.lastManual; //seting back the leds as the last manual (before updating the schedule)
 
   await system.save();
 
