705186e187f3682e58a395f17d7814bd29ab38f5
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-20 17:08:14 +0300

    Restructured the host middleware with the new hosts array

--- luminexa-server/middlewares/host.middleware.js
+++ luminexa-server/middlewares/host.middleware.js
@@ -1,5 +1,17 @@
-exports.hostMiddleware = async (req, res, next) => {
-  if (req.user.userType === "host") return next();
+const System = require("../models/system.model");
 
-  return res.status(401).json({ message: "Unauthorized" });
+exports.hostMiddleware = async (req, res, next) => {
+  const systemId = req.body.systemId;
+
+  const system = await System.findById(systemId);
+  if (!system) {
+    return res.status(404).json({ message: "System not found" });
+  }
+
+  const userId = req.user._id;
+  if (system.hosts.includes(userId)) {
+    return next();
+  } else {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 };
