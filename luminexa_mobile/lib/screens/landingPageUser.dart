47c1481db71cbc9a8df22342f12f2bf87d197777
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-23 21:25:44 +0300

    Created the structure app bar of the landing page

---
+++ luminexa_mobile/lib/screens/landingPageUser.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class landing extends StatefulWidget {
+  const landing({super.key});
+
+  @override
+  State<landing> createState() => _landingState();
+}
+
+class _landingState extends State<landing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "User Name",
+          style: TextStyle(
+              fontFamily: "Raleway", fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+          )
+        ],
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(25, 35, 0, 0),
+        child: Row(
+          children: [
+            Text(
+              "Systems",
+              style: TextStyle(
+                  fontFamily: "Raleway",
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
