ec01992d0f8b3c1fbce45ee04935cc7377976e81
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-24 16:05:37 +0300

    Frontend: Added a mode page structure with incoming test data handling

---
+++ luminexa_mobile/lib/screens/modePage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:luminexa_mobile/widgets/buttonWidget/iconButtonWidget.dart';
+import 'package:luminexa_mobile/widgets/listsWidget/toggleTileListWidget.dart';
+
+class modePage extends StatefulWidget {
+  const modePage({super.key});
+
+  @override
+  State<modePage> createState() => _modePageState();
+}
+
+class _modePageState extends State<modePage> {
+  final List modes = ["Dinner Mode", "Movie night mode", "Study mode"];
+  final List status = ["of", "on", "off"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+          child: Row(
+            children: [
+              Text(
+                "Modes",
+                style: TextStyle(fontFamily: "RalewayBold", fontSize: 20),
+              )
+            ],
+          ),
+        ),
+        ListView.builder(
+            shrinkWrap: true,
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return toggleTileList(
+                title: modes[index],
+                status: status[index],
+              );
+            }),
+      ],
+    )));
+  }
+}
