0004ea4ef4a352226370bde07cb8c4c9791d61a4
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-24 08:39:03 +0300

    Added a dynamic list tile widget that takes parameters

---
+++ luminexa_mobile/lib/widgets/listsWidget/listWidget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class listOption extends StatelessWidget {
+  final String systemName;
+  final int activeLeds;
+
+  const listOption({
+    super.key,
+    required this.systemName,
+    required this.activeLeds,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 75,
+      decoration: BoxDecoration(
+          border: Border(
+        top: BorderSide(color: Color.fromARGB(255, 173, 173, 173)),
+      )),
+      child: ListTile(
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            systemName,
+            style: TextStyle(
+              fontFamily: "RalewayNormal",
+              fontSize: 15,
+              color: Color.fromARGB(255, 63, 139, 0),
+            ),
+          ),
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "$activeLeds Active LEDS",
+            style: TextStyle(
+                fontFamily: "RalewayBold",
+                fontSize: 10,
+                fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
+    );
+  }
+}
