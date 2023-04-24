e851f20f2a17c3e907435609c5b749f6cb4dddbe
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-24 09:14:20 +0300

    Added a new leds list tile that handles name and status

---
+++ luminexa_mobile/lib/widgets/listsWidget/ledsListWidget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ledListOption extends StatelessWidget {
+  final String ledName;
+  final String status;
+
+  const ledListOption({
+    super.key,
+    required this.ledName,
+    required this.status,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 75,
+      decoration: BoxDecoration(
+          border: Border(
+        top: BorderSide(color: Color.fromARGB(255, 173, 173, 173)),
+      )),
+      child: ListTile(
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            ledName,
+            style: TextStyle(
+              fontFamily: "RalewayNormal",
+              fontSize: 15,
+              color: Color.fromARGB(255, 63, 139, 0),
+            ),
+          ),
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            "status $status",
+            style: TextStyle(
+                fontFamily: "RalewayBold",
+                fontSize: 10,
+                fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
+    );
+  }
+}
