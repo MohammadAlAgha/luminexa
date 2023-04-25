6b5f0010fa2d64fd6fc9f6b1157e374253659486
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-25 18:40:31 +0300

    Backend: structure spacing

--- luminexa-server/index.js
+++ luminexa-server/index.js
@@ -3,22 +3,30 @@ const app = express();
 require("dotenv").config();
 app.use(express.json());
 
+const { authMiddleware } = require("./middlewares/auth.middleware");
+const { hostMiddleware } = require("./middlewares/host.middleware");
+
 const authRouter = require("./routes/auth.routes");
 app.use("/auth", authRouter);
+
 const systemRouter = require("./routes/systems.routes");
-const { authMiddleware } = require("./middlewares/auth.middleware");
 app.use("/system", authMiddleware, systemRouter);
+
 const ledsRouter = require("./routes/leds.routes");
 app.use("/leds", authMiddleware, ledsRouter);
+
 const scheduleRouter = require("./routes/schedule.routes");
 app.use("/schedules", authMiddleware, scheduleRouter);
+
 const modeRouter = require("./routes/modes.routes");
 app.use("/modes", authMiddleware, modeRouter);
+
 const notificationRouter = require("./routes/notifications.routes");
 app.use("/notifications", authMiddleware, notificationRouter);
+
 const hostRouter = require("./routes/host.routes");
-const { hostMiddleware } = require("./middlewares/host.middleware");
 app.use("/host", authMiddleware, hostMiddleware, hostRouter);
+
 const historyRouter = require("./routes/history.routes");
 app.use("/history", authMiddleware, hostMiddleware, historyRouter);
 
