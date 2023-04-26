e105aab60153c6eb42eb6f0d76d193d83b5f7d85
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-26 16:54:55 +0300

    Frontend: Added scroll for the login and sign up

--- luminexa_mobile/lib/screens/login.dart
+++ luminexa_mobile/lib/screens/login.dart
@@ -18,96 +18,101 @@ class _logInState extends State<logIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SvgPicture.asset("images/Logo.svg"),
-              SizedBox(
-                height: 50,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(25),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Sign In",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: "RalewayBold",
-                                fontWeight: FontWeight.w900,
-                                fontSize: 22),
-                          ),
-                        ]),
-                  ),
-                  Container(
-                    height: 170,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          sytledTextField(
-                            controller: emailController,
-                            hintText: "Email",
-                            label: "Email",
-                            isPass: false,
-                          ),
-                          sytledTextField(
-                            controller: passwordController,
-                            hintText: "Password",
-                            label: "Password",
-                            isPass: true,
-                          ),
-                        ]),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Column(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                SvgPicture.asset("images/Logo.svg"),
+                SizedBox(
+                  height: 100,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    styledButton(innerText: "Sign In"),
-                    SizedBox(
-                      height: 20,
+                    Padding(
+                      padding: const EdgeInsets.all(25),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: "RalewayBold",
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 22),
+                            ),
+                          ]),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "No account? ",
-                          style: TextStyle(
-                              fontFamily: "Raleway",
-                              fontSize: 14,
-                              fontWeight: FontWeight.w800),
-                        ),
-                        Text.rich(TextSpan(
-                            style: TextStyle(
-                                fontFamily: "RalewayBold",
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                                color: Color.fromARGB(255, 63, 139, 0),
-                                decoration: TextDecoration.underline),
-                            text: "Sign Up",
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () async {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (BuildContext context) {
-                                  return SignUp();
-                                }));
-                              }))
-                      ],
-                    )
+                    Container(
+                      height: 200,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            sytledTextField(
+                              controller: emailController,
+                              hintText: "Email",
+                              label: "Email",
+                              isPass: false,
+                            ),
+                            sytledTextField(
+                              controller: passwordController,
+                              hintText: "Password",
+                              label: "Password",
+                              isPass: true,
+                            ),
+                          ]),
+                    ),
                   ],
                 ),
-              )
-            ]),
+                SizedBox(
+                  height: 180,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    children: [
+                      styledButton(innerText: "Sign In"),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "No account? ",
+                            style: TextStyle(
+                                fontFamily: "Raleway",
+                                fontSize: 14,
+                                fontWeight: FontWeight.w800),
+                          ),
+                          Text.rich(TextSpan(
+                              style: TextStyle(
+                                  fontFamily: "RalewayBold",
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color.fromARGB(255, 63, 139, 0),
+                                  decoration: TextDecoration.underline),
+                              text: "Sign Up",
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () async {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (BuildContext context) {
+                                    return SignUp();
+                                  }));
+                                }))
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ]),
+        ),
       ),
     );
   }
--- luminexa_mobile/lib/screens/signUp.dart
+++ luminexa_mobile/lib/screens/signUp.dart
@@ -21,108 +21,110 @@ class _SignUpState extends State<SignUp> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            SvgPicture.asset('images/Logo.svg'),
-            SizedBox(
-              height: 100,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Row(
-                children: [
-                  Text(
-                    "Sign Up",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: "RalewayBold",
-                        fontWeight: FontWeight.w900,
-                        fontSize: 22),
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 40,
               ),
-            ),
-            Container(
-              height: 300,
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+              SvgPicture.asset('images/Logo.svg'),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
                   children: [
-                    sytledTextField(
-                      controller: userNameController,
-                      label: "Name",
-                      hintText: "Name",
-                      isPass: false,
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: "RalewayBold",
+                          fontWeight: FontWeight.w900,
+                          fontSize: 22),
                     ),
-                    sytledTextField(
-                      controller: emailController,
-                      label: "Email",
-                      hintText: "Email",
-                      isPass: false,
-                    ),
-                    sytledTextField(
-                      controller: passwordController,
-                      label: "Password",
-                      hintText: "Password",
-                      isPass: true,
-                    ),
-                    sytledTextField(
-                      controller: ConfirmPasswordController,
-                      label: "Confirm Password",
-                      hintText: "Confirm Password",
-                      isPass: true,
-                    )
-                  ]),
-            ),
-            SizedBox(
-              height: 80,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: Column(
-                children: [
-                  styledButton(
-                    innerText: "Sign Up",
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Joined Us Before? ",
-                        style: TextStyle(
-                            fontFamily: "Raleway",
-                            fontSize: 14,
-                            fontWeight: FontWeight.w800),
-                      ),
-                      Text.rich(TextSpan(
-                          style: TextStyle(
-                              fontFamily: "RalewayBold",
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                              color: Color.fromARGB(255, 63, 139, 0),
-                              decoration: TextDecoration.underline),
-                          text: "Sign In",
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () async {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (BuildContext context) {
-                                return logIn();
-                              }));
-                            }))
-                    ],
-                  )
-                ],
+                  ],
+                ),
               ),
-            )
-          ],
+              Container(
+                height: 370,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      sytledTextField(
+                        controller: userNameController,
+                        label: "Name",
+                        hintText: "Name",
+                        isPass: false,
+                      ),
+                      sytledTextField(
+                        controller: emailController,
+                        label: "Email",
+                        hintText: "Email",
+                        isPass: false,
+                      ),
+                      sytledTextField(
+                        controller: passwordController,
+                        label: "Password",
+                        hintText: "Password",
+                        isPass: true,
+                      ),
+                      sytledTextField(
+                        controller: ConfirmPasswordController,
+                        label: "Confirm Password",
+                        hintText: "Confirm Password",
+                        isPass: true,
+                      )
+                    ]),
+              ),
+              SizedBox(
+                height: 55,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: Column(
+                  children: [
+                    styledButton(
+                      innerText: "Sign Up",
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Joined Us Before? ",
+                          style: TextStyle(
+                              fontFamily: "Raleway",
+                              fontSize: 14,
+                              fontWeight: FontWeight.w800),
+                        ),
+                        Text.rich(TextSpan(
+                            style: TextStyle(
+                                fontFamily: "RalewayBold",
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                                color: Color.fromARGB(255, 63, 139, 0),
+                                decoration: TextDecoration.underline),
+                            text: "Sign In",
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () async {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (BuildContext context) {
+                                  return logIn();
+                                }));
+                              }))
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
