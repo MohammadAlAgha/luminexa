286fe624b643e5439bc008644e121f6468286907
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-28 15:52:12 +0300

    Frontend: Added scaffold and body to the leds page

--- luminexa_mobile/lib/screens/ledPage.dart
+++ luminexa_mobile/lib/screens/ledPage.dart
@@ -13,31 +13,33 @@ class _ledsPageState extends State<ledsPage> {
   final status = ["ON", "OFF", "OFF", "ON"];
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-          child: Row(
-            children: [
-              Text(
-                "Edit LEDs in Kitchen",
-                style: TextStyle(
-                    fontFamily: "RalewayBold",
-                    fontSize: 18,
-                    fontWeight: FontWeight.w700),
-              )
-            ],
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+            child: Row(
+              children: [
+                Text(
+                  "Edit LEDs in Kitchen",
+                  style: TextStyle(
+                      fontFamily: "RalewayBold",
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700),
+                )
+              ],
+            ),
           ),
-        ),
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: leds.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ledListOption(ledName: leds[index], status: status[index]);
-          },
-        )
-      ],
-    ));
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: leds.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ledListOption(ledName: leds[index], status: status[index]);
+            },
+          )
+        ],
+      )),
+    );
   }
 }
