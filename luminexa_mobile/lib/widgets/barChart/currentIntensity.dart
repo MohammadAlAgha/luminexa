e1977be2edc5f3825f5ec89546e042bfaad67d4c
MohammadAlAgha <mohalagha11@gmail.com>
2023-04-29 19:24:07 +0300

    Frontend: Created current intensity class that will be displayed in graph

---
+++ luminexa_mobile/lib/widgets/barChart/currentIntensity.dart
@@ -0,0 +1,19 @@
+class currentIntensity {
+  final double IntensityHourOne;
+  final double IntensityHourTwo;
+  final double IntensityHourThree;
+  final double IntensityHourFour;
+  final double IntensityHourFive;
+  final double IntensityHourSix;
+  final double IntensityHourSeven;
+
+  currentIntensity({
+    required this.IntensityHourOne,
+    required this.IntensityHourTwo,
+    required this.IntensityHourThree,
+    required this.IntensityHourFour,
+    required this.IntensityHourFive,
+    required this.IntensityHourSix,
+    required this.IntensityHourSeven,
+  });
+}
