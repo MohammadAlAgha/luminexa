589392f5e6541fb6aa7cc6a2cd09cc96ebfa0d61
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-02 18:13:47 +0300

    Backend: Removed the edit led status route

--- luminexa-server/routes/leds.routes.js
+++ luminexa-server/routes/leds.routes.js
@@ -3,14 +3,12 @@ const router = Router();
 
 const {
   getLeds,
-  editLedStatus,
   editLed,
   addLed,
   getActiveLeds,
 } = require("../controllers/leds.controller");
 
 router.post("/getLeds", getLeds);
-router.put("/editLedStatus", editLedStatus);
 router.put("/editLed", editLed);
 router.post("/addled", addLed);
 router.get("/getActiveLeds", getActiveLeds);
