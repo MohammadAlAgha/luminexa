2d89fda43f38549f35e40fd6c5cf0bd57c3980ea
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-02 21:06:24 +0300

    Backend: Removed the apply mode route

--- luminexa-server/routes/modes.routes.js
+++ luminexa-server/routes/modes.routes.js
@@ -4,14 +4,13 @@ const router = Router();
 const {
   getModes,
   addMode,
-  applyMode,
+
   toggleMode,
   updateMode,
 } = require("../controllers/modes.controller");
 
 router.post("/getModes", getModes);
 router.post("/addMode", addMode);
-router.post("/applyMode", applyMode);
 router.put("/toggleMode", toggleMode);
 router.put("/updateMode", updateMode);
 
