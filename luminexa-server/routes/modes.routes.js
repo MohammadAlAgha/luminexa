b28d82fd4c494a507e329f1c6a4c2264b2cbeee2
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-02 21:57:26 +0300

    Backend: Added a route for the delete mode API

--- luminexa-server/routes/modes.routes.js
+++ luminexa-server/routes/modes.routes.js
@@ -4,13 +4,14 @@ const router = Router();
 const {
   getModes,
   addMode,
-
+  deleteMode,
   toggleMode,
   updateMode,
 } = require("../controllers/modes.controller");
 
 router.post("/getModes", getModes);
 router.post("/addMode", addMode);
+router.delete("/deleteMode", deleteMode);
 router.put("/toggleMode", toggleMode);
 router.put("/updateMode", updateMode);
 
