64308fecd4e2542a6f575f60832335afad36ce44
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-02 21:02:04 +0300

    Backend: Reworked the set mode status and removed apply mode API

--- luminexa-server/controllers/modes.controller.js
+++ luminexa-server/controllers/modes.controller.js
@@ -47,46 +47,6 @@ exports.addMode = async (req, res) => {
   res.json(system);
 };
 
-exports.applyMode = async (req, res) => {
-  const { systemId, modeId } = req.body;
-
-  const system = await System.findById(systemId); //finding the schedule in that system by ID
-
-  if (!system) {
-    return res.status(404).json({ message: "System not found" });
-  } //checking if system exists
-
-  const mode = system.modes.find((mode) => mode._id == modeId); //finding the mode in that system by ID
-
-  if (!mode) {
-    return res.status(404).json({ message: "Mode not found" });
-  } //checking if the mode exists
-
-  const notMode = system.modes.find((mode) => mode._id !== modeId); //getting all the non applied modes
-
-  system.modes.forEach((notMode) => {
-    if (notMode._id !== modeId) {
-      notMode.modeStatus = "off";
-    }
-  }); //makeing sure all the non applied modes are off
-
-  mode.modeStatus = "on"; //setting the applied mode as on
-
-  const leds = system.leds; //getting the systems leds
-
-  leds.forEach((led) => {
-    mode.leds.forEach((config) => {
-      if (led.id == config.leds) {
-        led.ledConfig = config;
-      }
-    });
-  }); //setting the leds status to match the saved leds status in the applied mode
-
-  await system.save();
-
-  res.json(system);
-};
-
 exports.toggleMode = async (req, res) => {
   const { systemId, modeId } = req.body;
 
@@ -104,8 +64,26 @@ exports.toggleMode = async (req, res) => {
 
   mode.modeStatus = mode.modeStatus == "on" ? "off" : "on"; //toggling the mode from on to off or from off to on
 
-  if (mode.modeStatus == "off") {
-    system.leds = system.lastManual; //Set the modes back to the old status after the mode is off
+  const leds = system.leds; //getting the systems leds
+  const originalConfigs = system.lastManual; //getting the system current leds last manual
+
+  if (mode.modeStatus == "on") {
+    leds.forEach((led) => {
+      mode.leds.forEach((config) => {
+        if (led.id == config.leds) {
+          led.ledConfig = config;
+        }
+      });
+    }); //setting the leds status to match the saved leds status in the applied mode
+  } else {
+    leds.forEach((led) => {
+      originalConfigs.forEach((config) => {
+        if (led.id == config.leds) {
+          led.ledConfig = config;
+        }
+      });
+    });
+    //retrieved the old status of the leds
   }
 
   await system.save();
