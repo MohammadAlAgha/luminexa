cc718fc43adb63e2d01387e425976f43b1860be6
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-03 21:18:18 +0300

    Frontend: Added the system model

---
+++ luminexa_mobile/lib/models/systemModel.dart
@@ -0,0 +1,24 @@
+import 'package:luminexa_mobile/models/ledModel.dart';
+import 'package:luminexa_mobile/models/modeModel.dart';
+import 'package:luminexa_mobile/models/scheduleModel.dart';
+import 'package:luminexa_mobile/models/userModel.dart';
+
+class System {
+  final List<User> hosts;
+  final List<User> users;
+  final String systemName;
+  final String serialNumber;
+  final List<Led> leds;
+  final List<Mode> modes;
+  final List<Schedule> schedule;
+
+  System({
+    required this.hosts,
+    required this.users,
+    required this.systemName,
+    required this.serialNumber,
+    required this.leds,
+    required this.modes,
+    required this.schedule,
+  });
+}
