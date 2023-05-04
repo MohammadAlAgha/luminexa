ea6fad95bf3027e79f0d15ef5c688e712e7a4428
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-04 06:51:02 +0300

    Frontend: Added a list of systems for the user model

--- luminexa_mobile/lib/models/userModel.dart
+++ luminexa_mobile/lib/models/userModel.dart
@@ -1,9 +1,13 @@
+import 'package:luminexa_mobile/models/systemModel.dart';
+
 class User {
   final String name;
   final String email;
+  final List<System> system;
 
   User({
     required this.name,
     required this.email,
+    required this.system,
   });
 }
