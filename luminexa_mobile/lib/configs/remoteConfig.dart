87224f157436ff6c8bea814aebf4d3d29b401ba4
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 15:03:45 +0300

    Frontend: Added the dio methods after token adding token to the headers

--- luminexa_mobile/lib/configs/remoteConfig.dart
+++ luminexa_mobile/lib/configs/remoteConfig.dart
@@ -1,8 +1,49 @@
 import 'package:dio/dio.dart';
+import 'package:luminexa_mobile/configs/local_storage_config.dart';
+import 'package:luminexa_mobile/enums/localTypes.dart';
+import 'package:luminexa_mobile/enums/requetMethods.dart';
 
 final options = BaseOptions(
-  baseUrl: "http://192.168.0.104:8000",
+  baseUrl: "http://192.168.0.102:8000",
   contentType: Headers.jsonContentType,
 );
 
 final dioClient = Dio(options);
+
+Future sendRequest({
+  required String route,
+  RequestMethods method = RequestMethods.GET,
+  Map? load,
+}) async {
+  final String? token =
+      await getLocal(type: LocalTypes.String, key: "access_token");
+
+  if (token != null) {
+    final BaseOptions authorizedOptions =
+        dioClient.options.copyWith(headers: {"Authorization": "Bearer $token"});
+
+    dioClient.options = authorizedOptions;
+  }
+
+  if (method == RequestMethods.GET) {
+    final response = await dioClient.get(route);
+
+    return response;
+  } else if (method == RequestMethods.POST) {
+    final response = await dioClient.post(route, data: load);
+
+    return response;
+  } else if (method == RequestMethods.PUT) {
+    final response = await dioClient.put(route, data: load);
+
+    return response;
+  } else if (method == RequestMethods.DELETE) {
+    final response = await dioClient.delete(route, data: load);
+
+    return response;
+  }
+
+  return Future(() {
+    return "Not a valid method";
+  });
+}
