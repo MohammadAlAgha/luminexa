bb9908fa3b7f086bdc58b45d71e43ed3c1ff7e18
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 21:17:20 +0300

    Frontend: Added the fromJson function to parse mode data

--- luminexa_mobile/lib/providers/ModesProvider.dart
+++ luminexa_mobile/lib/providers/ModesProvider.dart
@@ -1,29 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:luminexa_mobile/APIs/ModeAPIs.dart';
+import 'package:luminexa_mobile/models/modeModel.dart';
 
 class ModesProvider extends ChangeNotifier {
-  static Future getModes(systemId) async {
-    final Response = await ModeAPIs.getModes(systemId);
-    return Response;
+  List<Mode> modes = [];
+
+  ModesProvider({
+    required this.modes,
+  });
+
+  Future getModes(systemId) async {
+    final response = await ModeAPIs.getModes(systemId);
+    return response;
   }
 
-  static Future addMode(systemId, modeName) async {
-    final Response = await ModeAPIs.addMode(systemId, modeName);
-    return Response;
+  Future addMode(systemId, modeName) async {
+    final response = await ModeAPIs.addMode(systemId, modeName);
+    return response;
   }
 
-  static Future toggleMode(systemId, modeId) async {
-    final Response = await ModeAPIs.toggleMode(systemId, modeId);
-    return Response;
+  Future toggleMode(systemId, modeId) async {
+    final response = await ModeAPIs.toggleMode(systemId, modeId);
+    return response;
   }
 
-  static Future updateMode(systemId, modeId, modeName) async {
-    final Response = await ModeAPIs.updateMode(systemId, modeId, modeName);
-    return Response;
+  Future updateMode(systemId, modeId, modeName) async {
+    final response = await ModeAPIs.updateMode(systemId, modeId, modeName);
+    return response;
   }
 
-  static Future deleteMode(systemId, modeId) async {
-    final Response = await ModeAPIs.deleteMode(systemId, modeId);
-    return Response;
+  Future deleteMode(systemId, modeId) async {
+    final response = await ModeAPIs.deleteMode(systemId, modeId);
+    return response;
+  }
+
+  Mode fromJSON(Map json) {
+    final Mode newMode = Mode(
+        id: json["id"],
+        modeName: json["modeName"],
+        leds: json["leds"],
+        modeStatus: json['modeStatus']);
+    return newMode;
   }
 }
