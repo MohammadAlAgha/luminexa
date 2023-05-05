d0db5ee7d60144372035822d46d2b7b38f356305
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 18:43:22 +0300

    Frontend: Added the modes methods in the modes provider

--- luminexa_mobile/lib/providers/ModesProvider.dart
+++ luminexa_mobile/lib/providers/ModesProvider.dart
@@ -6,4 +6,24 @@ class ModesProvider extends ChangeNotifier {
     final Response = await ModeAPIs.getModes(systemId);
     return Response;
   }
+
+  static Future addMode(systemId, modeName) async {
+    final Response = await ModeAPIs.addMode(systemId, modeName);
+    return Response;
+  }
+
+  static Future toggleMode(systemId, modeId) async {
+    final Response = await ModeAPIs.toggleMode(systemId, modeId);
+    return Response;
+  }
+
+  static Future updateMode(systemId, modeId, modeName) async {
+    final Response = await ModeAPIs.updateMode(systemId, modeId, modeName);
+    return Response;
+  }
+
+  static Future deleteMode(systemId, modeId) async {
+    final Response = await ModeAPIs.deleteMode(systemId, modeId);
+    return Response;
+  }
 }
