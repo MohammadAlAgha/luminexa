ac29fe7b925780e377190636f353639f4805d765
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 21:17:40 +0300

    Frontend: Added the fromJson function to parse notification data

--- luminexa_mobile/lib/providers/NotificationsProvider.dart
+++ luminexa_mobile/lib/providers/NotificationsProvider.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:luminexa_mobile/APIs/NotificationsAPI.dart';
+import 'package:luminexa_mobile/models/notificationModel.dart';
 
 class NotificationsProvider extends ChangeNotifier {
-  static Future getSystemNotitifications(systemId) async {
-    final Response = await NotificationsAPIs.getSystemNotitifications(systemId);
-    return Response;
+  List<Notifications> notifications = [];
+
+  NotificationsProvider({
+    required this.notifications,
+  });
+
+  Future getSystemNotitifications(systemId) async {
+    final response = await NotificationsAPIs.getSystemNotitifications(systemId);
+    return response;
   }
 
-  static Future createNotifications(time, systemId, description) async {
-    final Response = await NotificationsAPIs.createNotifications(
+  Future createNotifications(time, systemId, description) async {
+    final response = await NotificationsAPIs.createNotifications(
         time, systemId, description);
-    return Response;
+    return response;
+  }
+
+  Notifications fromJSON(Map json) {
+    final newNotification = Notifications(
+        time: json['time'],
+        systemId: json['systemId'],
+        description: json['description']);
+    return newNotification;
   }
 }
