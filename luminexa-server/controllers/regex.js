538841164290251904f229b3d7947dda05a98165
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 16:38:55 +0300

    Backend: Added the regex validation file

---
+++ luminexa-server/controllers/regex.js
@@ -0,0 +1,11 @@
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  return passwordRegex.test(password);
+}
+
+module.exports = { validateEmail, validatePassword };
