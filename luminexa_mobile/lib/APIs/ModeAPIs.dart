53bae2e160a3d5d98d8f79f936a14c00433418dd
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 18:18:44 +0300

    Frontend: Reworked the get modes API call

--- luminexa_mobile/lib/APIs/ModeAPIs.dart
+++ luminexa_mobile/lib/APIs/ModeAPIs.dart
@@ -1,11 +1,13 @@
 import 'package:luminexa_mobile/configs/remoteConfig.dart';
+import 'package:luminexa_mobile/enums/requestMethods.dart';
 
 class ModeAPIs {
   static Future getModes(systemId) async {
     final body = {"systemId": systemId};
     try {
-      final Response = await dioClient.post("/mode/getModes", data: body);
-      return Response;
+      final response = await sendRequest(
+          route: "/mode/getModes", method: RequestMethods.GET, load: body);
+      return response;
     } catch (e) {
       rethrow;
     }
