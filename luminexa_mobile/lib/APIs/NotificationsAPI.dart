52e12fc8b7b807c00487bb2a57076360ad17611d
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 18:16:35 +0300

    Frontend: Added the create notification API

--- luminexa_mobile/lib/APIs/NotificationsAPI.dart
+++ luminexa_mobile/lib/APIs/NotificationsAPI.dart
@@ -1,11 +1,31 @@
 import 'package:luminexa_mobile/configs/remoteConfig.dart';
+import 'package:luminexa_mobile/enums/requestMethods.dart';
 
 class NotificationsAPIs {
   static Future getSystemNotitifications(systemId) async {
     final body = {"systemId": systemId};
     try {
-      final Response = await dioClient
-          .post("/notifications/getSystemNotitifications", data: body);
+      final Response = await sendRequest(
+          route: "/schedules/getSchedules",
+          method: RequestMethods.GET,
+          load: body);
+      return Response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  static Future createNotifications(time, systemId, description) async {
+    final body = {
+      "systemId": systemId,
+      "time": time,
+      "description": description
+    };
+    try {
+      final Response = await sendRequest(
+          route: "/schedules/createNotifications",
+          method: RequestMethods.POST,
+          load: body);
       return Response;
     } catch (e) {
       rethrow;
