3896b01ae52f23201eda41061e0d1e8f8f9f2ab1
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 16:38:11 +0300

    Backend: Fixed the routing types for the system routes

--- luminexa-server/routes/systems.routes.js
+++ luminexa-server/routes/systems.routes.js
@@ -4,6 +4,6 @@ const router = Router();
 const { addSystem, getSystems } = require("../controllers/systems.controller");
 
 router.post("/addSystem", addSystem);
-router.post("/getSystems", getSystems);
+router.get("/getSystems", getSystems);
 
 module.exports = router;
