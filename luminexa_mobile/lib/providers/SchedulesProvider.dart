537552cdc30ba06edf31551402ca4dd2396a3d01
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-05 20:24:46 +0300

    Frontend: Added time start and time end for the schedule API calls

--- luminexa_mobile/lib/providers/SchedulesProvider.dart
+++ luminexa_mobile/lib/providers/SchedulesProvider.dart
@@ -6,4 +6,28 @@ class SchedulesProvider extends ChangeNotifier {
     final Response = await ScheduleAPIs.getSchedules(systemId);
     return Response;
   }
+
+  static Future addSchedule(
+      systemId, scheduleTitle, timeStart, timeEnd, repeat) async {
+    final Response = await ScheduleAPIs.addSchedule(
+        systemId, scheduleTitle, timeStart, timeEnd, repeat);
+    return Response;
+  }
+
+  static Future toggleSchedule(systemId, scheduleId) async {
+    final Response = await ScheduleAPIs.toggleSchedule(systemId, scheduleId);
+    return Response;
+  }
+
+  static Future updateSchedule(
+      systemId, scheduleId, scheduleTitle, timeStart, timeEnd, repeat) async {
+    final Response = await ScheduleAPIs.updateSchedule(
+        systemId, scheduleId, scheduleTitle, timeStart, timeEnd, repeat);
+    return Response;
+  }
+
+  static Future deleteSchedule(systemId, scheduleId) async {
+    final Response = await ScheduleAPIs.deleteSchedule(systemId, scheduleId);
+    return Response;
+  }
 }
