1a587a4b8ce485ee75d5542a34b7ace98afafb0c
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-06 23:23:04 +0300

    Frontend: Fetched the leds list in the set schedule page

--- luminexa_mobile/lib/screens/setSchedulePage.dart
+++ luminexa_mobile/lib/screens/setSchedulePage.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:luminexa_mobile/models/ledModel.dart';
+import 'package:luminexa_mobile/providers/LedsProvider.dart';
 import 'package:luminexa_mobile/widgets/appBarWidget/appBarWidget.dart';
 import 'package:luminexa_mobile/widgets/authWidgets/authWidgets.dart';
 import 'package:luminexa_mobile/widgets/buttonWidget/buttonWidget.dart';
 import 'package:luminexa_mobile/widgets/buttonWidget/customeCheckBox.dart';
 import 'package:luminexa_mobile/widgets/listsWidget/ledsListWidget.dart';
 import 'package:luminexa_mobile/widgets/titleWidget/titleWidget.dart';
+import 'package:provider/provider.dart';
 
 class SetSchedulePage extends StatefulWidget {
-  const SetSchedulePage({super.key});
+  final String systemId;
+  const SetSchedulePage({
+    super.key,
+    required this.systemId,
+  });
   @override
   State<SetSchedulePage> createState() => _SetSchedulePageState();
 }
 
 class _SetSchedulePageState extends State<SetSchedulePage> {
+  Future<void> fetchLeds() async {
+    await Provider.of<LedsProvider>(context, listen: false)
+        .getLeds(widget.systemId);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchLeds();
+  }
+
   final scheduleName = TextEditingController();
   TimeOfDay timeToStart = TimeOfDay(hour: 12, minute: 0);
   TimeOfDay timeToEnd = TimeOfDay(hour: 1, minute: 0);
@@ -35,105 +53,115 @@ class _SetSchedulePageState extends State<SetSchedulePage> {
     });
   }
 
-  final leds = ["LED 1", "LED 2", "LED 3", "LED 4"];
-  final status = ["ON", "OFF", "OFF", "ON"];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar(
-        title: "Set Schedule",
-        iconLeading: Icon(Icons.arrow_back),
-        iconLeadingFunction: () {
-          Navigator.of(context).pop(context);
-        },
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            titleWidget(title: "Title of the schedule"),
-            SizedBox(
-              height: 10,
+    return Consumer<LedsProvider>(
+      builder: (context, value, child) {
+        List<Led> _leds = value.leds;
+        return Scaffold(
+          appBar: appBar(
+            title: "Set Schedule",
+            iconLeading: Icon(Icons.arrow_back),
+            iconLeadingFunction: () {
+              Navigator.of(context).pop(context);
+            },
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                titleWidget(title: "Title of the schedule"),
+                SizedBox(
+                  height: 10,
+                ),
+                sytledTextField(
+                  isPass: false,
+                  controller: scheduleName,
+                  label: "Title",
+                  hintText: "Title",
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                titleWidget(title: "Time"),
+                Text(
+                  timeToStart.format(context),
+                  style: TextStyle(
+                      fontFamily: "ralewayBold",
+                      fontSize: 25,
+                      color: Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: styledButton(
+                    innerText: "Pick a time to start",
+                    onTap: showTimeStart,
+                  ),
+                ),
+                Text(
+                  timeToEnd.format(context),
+                  style: TextStyle(
+                      fontFamily: "ralewayBold",
+                      fontSize: 25,
+                      color: Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: styledButton(
+                    innerText: "Pick a time to end",
+                    onTap: showTimeEnd,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                titleWidget(title: "Repeat"),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      customeCheckBox(day: "Mon", isPressed: false),
+                      customeCheckBox(day: "Tue", isPressed: false),
+                      customeCheckBox(day: "Wed", isPressed: false),
+                      customeCheckBox(day: "Thu", isPressed: false),
+                      customeCheckBox(day: "Fri", isPressed: false),
+                      customeCheckBox(day: "Sat", isPressed: false),
+                      customeCheckBox(day: "Sun", isPressed: false),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                titleWidget(title: "LEDs in Kitchen"),
+                SizedBox(
+                  height: 10,
+                ),
+                ListView.builder(
+                  physics: ScrollPhysics(parent: null),
+                  shrinkWrap: true,
+                  itemCount: _leds.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ledListOption(
+                        led: _leds[index],
+                        ledName: _leds[index].ledName,
+                        status: _leds[index].ledStatus);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: styledButton(
+                    innerText: "Set Schedule",
+                    onTap: () {},
+                  ),
+                )
+              ],
             ),
-            sytledTextField(
-              isPass: false,
-              controller: scheduleName,
-              label: "Title",
-              hintText: "Title",
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            titleWidget(title: "Time"),
-            Text(
-              timeToStart.format(context),
-              style: TextStyle(
-                  fontFamily: "ralewayBold", fontSize: 25, color: Colors.black),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: styledButton(
-                innerText: "Pick a time to start",
-                onTap: showTimeStart,
-              ),
-            ),
-            Text(
-              timeToEnd.format(context),
-              style: TextStyle(
-                  fontFamily: "ralewayBold", fontSize: 25, color: Colors.black),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: styledButton(
-                innerText: "Pick a time to end",
-                onTap: showTimeEnd,
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            titleWidget(title: "Repeat"),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  customeCheckBox(day: "Mon", isPressed: false),
-                  customeCheckBox(day: "Tue", isPressed: false),
-                  customeCheckBox(day: "Wed", isPressed: false),
-                  customeCheckBox(day: "Thu", isPressed: false),
-                  customeCheckBox(day: "Fri", isPressed: false),
-                  customeCheckBox(day: "Sat", isPressed: false),
-                  customeCheckBox(day: "Sun", isPressed: false),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            titleWidget(title: "LEDs in Kitchen"),
-            SizedBox(
-              height: 10,
-            ),
-            // ListView.builder(
-            //   physics: ScrollPhysics(parent: null),
-            //   shrinkWrap: true,
-            //   itemCount: leds.length,
-            //   itemBuilder: (BuildContext context, int index) {
-            //     return ledListOption(
-            //         ledName: leds[index], status: status[index]);
-            //   },
-            // ),
-            Padding(
-              padding: const EdgeInsets.all(25),
-              child: styledButton(
-                innerText: "Set Schedule",
-                onTap: () {},
-              ),
-            )
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
