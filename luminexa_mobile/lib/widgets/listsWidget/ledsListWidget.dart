dfc70c9934df40f99c212665aba635a22dd6bddd
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-06 22:57:34 +0300

    Frontend: Added gesture detector for the led list tile

--- luminexa_mobile/lib/widgets/listsWidget/ledsListWidget.dart
+++ luminexa_mobile/lib/widgets/listsWidget/ledsListWidget.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:luminexa_mobile/models/ledModel.dart';
+import 'package:luminexa_mobile/routes/routes.dart';
 
 class ledListOption extends StatelessWidget {
   final String ledName;
   final String status;
+  final Led led;
 
   const ledListOption({
     super.key,
     required this.ledName,
     required this.status,
+    required this.led,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      decoration: BoxDecoration(
-          border: Border(
-        top: BorderSide(color: Color.fromARGB(255, 173, 173, 173)),
-      )),
-      child: ListTile(
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Text(
-            ledName,
-            style: Theme.of(context).textTheme.titleLarge,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(RouteManager.editLedPage, arguments: {
+          "led": led,
+        });
+      },
+      child: Container(
+        height: 75,
+        decoration: BoxDecoration(
+            border: Border(
+          top: BorderSide(color: Color.fromARGB(255, 173, 173, 173)),
+        )),
+        child: ListTile(
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              ledName,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
           ),
-        ),
-        subtitle: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Text("status $status",
-              style: Theme.of(context).textTheme.labelMedium),
+          subtitle: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text("status $status",
+                style: Theme.of(context).textTheme.labelMedium),
+          ),
         ),
       ),
     );
