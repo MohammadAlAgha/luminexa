294eea75aaf2fd6d33f6239b93da4a1663f8b330
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-06 11:09:58 +0300

    Frontend: Passed the system Id to the Led Page

--- luminexa_mobile/lib/screens/systemPage.dart
+++ luminexa_mobile/lib/screens/systemPage.dart
@@ -8,80 +8,93 @@ import 'package:luminexa_mobile/screens/SchedulePage.dart';
 import 'package:luminexa_mobile/widgets/appBarWidget/appBarWidget.dart';
 
 class SystemPage extends StatefulWidget {
-  const SystemPage({super.key});
+  final String systemId;
+
+  const SystemPage({
+    super.key,
+    required this.systemId,
+  });
 
   @override
   State<SystemPage> createState() => _SystemPageState();
 }
 
 class _SystemPageState extends State<SystemPage> {
-  @override
   int currentIndex = 0;
+
   void navigateTab(index) {
     setState(() {
       currentIndex = index;
     });
   }
 
-  final List pages = [
-    LedsPage(),
-    SchedulePage(),
-    ModePage(),
-    NotificationsPage()
-  ];
+  Widget getPage() {
+    final List pages = [
+      LedsPage(systemId: widget.systemId),
+      SchedulePage(),
+      ModePage(),
+      NotificationsPage()
+    ];
+
+    return pages[currentIndex];
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(25))),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
-            child: GNav(
-              activeColor: Theme.of(context).primaryColor,
-              onTabChange: (value) => navigateTab(value),
-              backgroundColor: Theme.of(context).primaryColor,
-              tabBackgroundColor: Colors.white,
-              color: Colors.white,
-              gap: 10,
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-              tabs: [
-                GButton(
-                  icon: Icons.lightbulb,
-                  iconSize: 20,
-                  text: "LEDs",
-                  textStyle: Theme.of(context).textTheme.headlineLarge,
-                ),
-                GButton(
-                  icon: Icons.schedule,
-                  iconSize: 20,
-                  text: "Schedules",
-                  textStyle: Theme.of(context).textTheme.headlineLarge,
-                ),
-                GButton(
-                  icon: Icons.list,
-                  iconSize: 20,
-                  text: "Modes",
-                  textStyle: Theme.of(context).textTheme.headlineLarge,
-                ),
-                GButton(
-                  icon: Icons.notifications,
-                  iconSize: 20,
-                  text: "Notifications",
-                  textStyle: Theme.of(context).textTheme.headlineLarge,
-                ),
-              ],
-            ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(25))),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+          child: GNav(
+            activeColor: Theme.of(context).primaryColor,
+            onTabChange: (value) => navigateTab(value),
+            backgroundColor: Theme.of(context).primaryColor,
+            tabBackgroundColor: Colors.white,
+            color: Colors.white,
+            gap: 10,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            tabs: [
+              GButton(
+                icon: Icons.lightbulb,
+                iconSize: 20,
+                text: "LEDs",
+                textStyle: Theme.of(context).textTheme.headlineLarge,
+              ),
+              GButton(
+                icon: Icons.schedule,
+                iconSize: 20,
+                text: "Schedules",
+                textStyle: Theme.of(context).textTheme.headlineLarge,
+              ),
+              GButton(
+                icon: Icons.list,
+                iconSize: 20,
+                text: "Modes",
+                textStyle: Theme.of(context).textTheme.headlineLarge,
+              ),
+              GButton(
+                icon: Icons.notifications,
+                iconSize: 20,
+                text: "Notifications",
+                textStyle: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ],
           ),
         ),
-        appBar: appBar(
-          title: "Kitchen",
-          iconAction: Icon(Icons.settings),
-          iconActionFunction: () =>
-              Navigator.of(context).pushNamed(RouteManager.settingsPage),
-          iconLeading: Icon(Icons.arrow_back),
-          iconLeadingFunction: () => Navigator.of(context).pop(context),
-        ),
-        body: pages[currentIndex]);
+      ),
+      appBar: appBar(
+        title: "Kitchen",
+        iconAction: Icon(Icons.settings),
+        iconActionFunction: () =>
+            Navigator.of(context).pushNamed(RouteManager.settingsPage),
+        iconLeading: Icon(Icons.arrow_back),
+        iconLeadingFunction: () => Navigator.of(context).pop(context),
+      ),
+      // body: pages[currentIndex],
+      body: getPage(),
+    );
   }
 }
