4e92d126a38269186b902fd2903cc9b6ff8598cd
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-06 23:14:39 +0300

    Frontend: Fetched the led list in the set mode page

--- luminexa_mobile/lib/screens/setModePage.dart
+++ luminexa_mobile/lib/screens/setModePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:luminexa_mobile/models/ledModel.dart';
 import 'package:luminexa_mobile/providers/LedsProvider.dart';
 import 'package:luminexa_mobile/widgets/appBarWidget/appBarWidget.dart';
 import 'package:luminexa_mobile/widgets/authWidgets/authWidgets.dart';
@@ -7,57 +8,86 @@ import 'package:luminexa_mobile/widgets/listsWidget/ledsListWidget.dart';
 import 'package:luminexa_mobile/widgets/titleWidget/titleWidget.dart';
 import 'package:provider/provider.dart';
 
-class SetModePage extends StatelessWidget {
-  SetModePage({super.key});
+class SetModePage extends StatefulWidget {
+  final String systemId;
 
+  SetModePage({
+    super.key,
+    required this.systemId,
+  });
+
+  @override
+  State<SetModePage> createState() => _SetModePageState();
+}
+
+class _SetModePageState extends State<SetModePage> {
   final modeController = TextEditingController();
+
+  Future<void> fetchLeds() async {
+    await Provider.of<LedsProvider>(context, listen: false)
+        .getLeds(widget.systemId);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchLeds();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar(
-        title: "Set Mode",
-        iconLeading: Icon(Icons.arrow_back),
-        iconLeadingFunction: () {
-          Navigator.pop(context);
-        },
-      ),
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          titleWidget(title: "Title of the schedule"),
-          SizedBox(
-            height: 10,
+    return Consumer<LedsProvider>(
+      builder: (context, value, child) {
+        List<Led> _leds = value.leds;
+        return Scaffold(
+          appBar: appBar(
+            title: "Set Mode",
+            iconLeading: Icon(Icons.arrow_back),
+            iconLeadingFunction: () {
+              Navigator.pop(context);
+            },
           ),
-          sytledTextField(
-              isPass: false,
-              controller: modeController,
-              label: "Title",
-              hintText: "Title"),
-          SizedBox(
-            height: 30,
-          ),
-          titleWidget(title: "LEDs in Kitchen"),
-          SizedBox(
-            height: 10,
-          ),
-          // ListView.builder(
-          //   physics: ScrollPhysics(parent: null),
-          //   shrinkWrap: true,
-          //   itemCount: leds.length,
-          //   itemBuilder: (BuildContext context, int index) {
-          //     return ledListOption(ledName: leds[index], status: status[index]);
-          //   },
-          // ),
-          Padding(
-            padding: const EdgeInsets.all(25),
-            child: styledButton(
-              innerText: "Set Mode",
-              onTap: () {},
-            ),
-          )
-        ],
-      )),
+          body: SingleChildScrollView(
+              child: Column(
+            children: [
+              titleWidget(title: "Title of the schedule"),
+              SizedBox(
+                height: 10,
+              ),
+              sytledTextField(
+                  isPass: false,
+                  controller: modeController,
+                  label: "Title",
+                  hintText: "Title"),
+              SizedBox(
+                height: 30,
+              ),
+              titleWidget(title: "LEDs in Kitchen"),
+              SizedBox(
+                height: 10,
+              ),
+              ListView.builder(
+                physics: ScrollPhysics(parent: null),
+                shrinkWrap: true,
+                itemCount: _leds.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ledListOption(
+                      led: _leds[index],
+                      ledName: _leds[index].ledName,
+                      status: _leds[index].ledStatus);
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: styledButton(
+                  innerText: "Set Mode",
+                  onTap: () {},
+                ),
+              )
+            ],
+          )),
+        );
+      },
     );
   }
 }
