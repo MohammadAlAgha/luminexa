8fa696e36205c79766f6981ead5f2d6ce6797e27
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-06 22:10:34 +0300

    Frontend: Fixed the data types in the notification model

--- luminexa_mobile/lib/models/notificationModel.dart
+++ luminexa_mobile/lib/models/notificationModel.dart
@@ -2,14 +2,14 @@ import 'package:luminexa_mobile/models/systemModel.dart';
 
 class Notifications {
   final String id;
-  final DateTime time;
-  final System systemId;
+  final String time;
+  final String system;
   final String description;
 
   Notifications({
     required this.id,
     required this.time,
-    required this.systemId,
+    required this.system,
     required this.description,
   });
 }
