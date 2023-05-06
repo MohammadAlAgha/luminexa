14fc7fb9c901558b5e25813c48ce78756a50867f
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-06 11:06:23 +0300

    Frontend: Added guesture detector with IDs for the systems list tile

--- luminexa_mobile/lib/widgets/listsWidget/listWidget.dart
+++ luminexa_mobile/lib/widgets/listsWidget/listWidget.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:luminexa_mobile/models/systemModel.dart';
+import 'package:luminexa_mobile/routes/routes.dart';
 
 class listOption extends StatelessWidget {
-  final String systemName;
+  final System system;
   final int activeLeds;
 
   const listOption({
     super.key,
-    required this.systemName,
+    required this.system,
     required this.activeLeds,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      decoration: BoxDecoration(
-          border: Border(
-        top: BorderSide(color: Color.fromARGB(255, 173, 173, 173)),
-      )),
-      child: ListTile(
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Text(
-            systemName,
-            style: Theme.of(context).textTheme.titleLarge,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(RouteManager.systemPage, arguments: {
+          "systemId": system.id,
+        });
+      },
+      child: Container(
+        height: 75,
+        decoration: BoxDecoration(
+            border: Border(
+          top: BorderSide(color: Color.fromARGB(255, 173, 173, 173)),
+        )),
+        child: ListTile(
+          title: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              system.systemName,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
           ),
-        ),
-        subtitle: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Text("$activeLeds Active LEDS",
-              style: Theme.of(context).textTheme.labelLarge),
+          subtitle: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text("$activeLeds Active LEDS",
+                style: Theme.of(context).textTheme.labelLarge),
+          ),
         ),
       ),
     );
