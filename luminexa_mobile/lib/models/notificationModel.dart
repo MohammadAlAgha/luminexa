78a03f773a56dae6233cf909d3a8bfa00bc95074
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-06 14:58:37 +0300

    Frontend: Fixed the ID data type in the notifications model

--- luminexa_mobile/lib/models/notificationModel.dart
+++ luminexa_mobile/lib/models/notificationModel.dart
@@ -1,11 +1,13 @@
 import 'package:luminexa_mobile/models/systemModel.dart';
 
 class Notifications {
+  final String id;
   final DateTime time;
   final System systemId;
   final String description;
 
   Notifications({
+    required this.id,
     required this.time,
     required this.systemId,
     required this.description,
