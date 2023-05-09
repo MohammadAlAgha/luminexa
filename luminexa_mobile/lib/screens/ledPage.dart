937d7240ef814e70053514fecc7e07e7d0a8fdb5
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-09 09:46:29 +0300

    Frontend: Fetched the leds by system provider in the leds page

--- luminexa_mobile/lib/screens/ledPage.dart
+++ luminexa_mobile/lib/screens/ledPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:luminexa_mobile/models/ledModel.dart';
+import 'package:luminexa_mobile/models/systemModel.dart';
 import 'package:luminexa_mobile/providers/LedsProvider.dart';
+import 'package:luminexa_mobile/providers/SystemsProvider.dart';
 import 'package:luminexa_mobile/widgets/listsWidget/ledsListWidget.dart';
 import 'package:luminexa_mobile/widgets/titleWidget/titleWidget.dart';
 import 'package:provider/provider.dart';
@@ -20,21 +22,14 @@ class LedsPage extends StatefulWidget {
 }
 
 class _LedsPageState extends State<LedsPage> {
-  Future<void> fetchLeds() async {
-    await Provider.of<LedsProvider>(context, listen: false)
-        .getLeds(widget.systemId);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchLeds();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Consumer<LedsProvider>(builder: (context, value, child) {
-      List<Led> _leds = value.leds;
+    return Consumer<SystemsProvider>(builder: (context, value, child) {
+      List<Led> _leds = value.systems
+          .firstWhere(
+            (system) => system.id == widget.systemId,
+          )
+          .leds;
 
       return SafeArea(
         child: SingleChildScrollView(
