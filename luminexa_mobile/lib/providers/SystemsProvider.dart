9693ac9343140d70c616c530e7214b00f771a466
MohammadAlAgha <mohalagha11@gmail.com>
2023-05-09 09:45:42 +0300

    Udjusted the from json in the systems provider

--- luminexa_mobile/lib/providers/SystemsProvider.dart
+++ luminexa_mobile/lib/providers/SystemsProvider.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:luminexa_mobile/APIs/SystemAPIs.dart';
+import 'package:luminexa_mobile/models/ledModel.dart';
+import 'package:luminexa_mobile/models/modeModel.dart';
+import 'package:luminexa_mobile/models/scheduleModel.dart';
 import 'package:luminexa_mobile/models/systemModel.dart';
+import 'package:luminexa_mobile/providers/LedsProvider.dart';
+import 'package:luminexa_mobile/providers/ModesProvider.dart';
+import 'package:luminexa_mobile/providers/SchedulesProvider.dart';
+import 'package:provider/provider.dart';
 
 class SystemsProvider extends ChangeNotifier {
   List<System> systems;
@@ -14,7 +21,7 @@ class SystemsProvider extends ChangeNotifier {
 
     List<System> _systems = [];
 
-    response.data.forEach((map) {
+    response.data["systems"].forEach((map) {
       final System system = fromJSON(map);
       _systems.add(system);
     });
@@ -28,7 +35,7 @@ class SystemsProvider extends ChangeNotifier {
     final response = await SystemAPIs.addSystem(serialNumber);
     List<System> _systems = [];
 
-    response.data.forEach((map) {
+    response.data["systems"].forEach((map) {
       final System system = fromJSON(map);
       _systems.add(system);
     });
@@ -38,16 +45,40 @@ class SystemsProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  void updateLed(String systemId, Led led) {
+    final System system = systems.firstWhere((system) => systemId == system.id);
+
+    for (var i = 0; i < system.leds.length; i++) {
+      if (system.leds[i].id == led.id) {
+        system.leds[i] = led;
+      }
+    }
+
+    notifyListeners();
+  }
+
   System fromJSON(Map json) {
+    List<Schedule> parsedSchedules = [];
+    json["schedules"].forEach((schedule) {
+      parsedSchedules.add(SchedulesProvider.fromJSON(schedule));
+    });
+    List<Mode> parsedModes = [];
+    json["modes"].forEach((mode) {
+      parsedModes.add(ModesProvider.fromJSON(mode));
+    });
+    List<Led> parsedLeds = [];
+    json["leds"].forEach((led) {
+      parsedLeds.add(LedsProvider.fromJSON(led));
+    });
     final System newSystem = System(
       id: json["_id"],
       hosts: [],
       users: [],
       systemName: json["systemName"],
       serialNumber: json["serialNumber"],
-      leds: [],
-      modes: [],
-      schedule: [],
+      leds: parsedLeds,
+      modes: parsedModes,
+      schedules: parsedSchedules,
     );
 
     return newSystem;
